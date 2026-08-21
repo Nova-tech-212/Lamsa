733eaf46c20db585f71524def48be19f6a95b625
Achraf <achrafabouziad@gmail.com>
2026-08-21 21:14:21 +0100

    Update vite config for relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This allows the built files to be opened directly or hosted anywhere
 })
